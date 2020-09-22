51b427ee343340e5b49070cd9f20d3a75da97b02
Aleksandra Toncheva <aleksandra_toncheva@yahoo.com>
2020-09-22 12:23:39 +0300

    Add files to DefiningClasses

---
+++ DefiningClasses/SoftUniParking/Car.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SoftUniParking
+{
+    public class Car
+    {
+
+        public Car(string make, string model, int horsePower, string registrationNumber)
+        {
+            this.Make = make;
+            this.Model = model;
+            this.HorsePower = horsePower;
+            this.RegistrationNumber = registrationNumber;
+        }
+        private string make;
+
+        public string Make
+        {
+            get { return make; }
+            set { make = value; }
+        }
+        private string model;
+
+        public string Model
+        {
+            get { return model; }
+            set { model = value; }
+        }
+
+
+        private int horsePower;
+
+        public int HorsePower
+        {
+            get { return horsePower; }
+            set { horsePower = value; }
+        }
+        private string registrationNumber;
+
+        public string RegistrationNumber
+        {
+            get { return registrationNumber; }
+            set { registrationNumber = value; }
+        }
+
+        public override string ToString()
+        {
+            var sb = new StringBuilder();
+            sb.AppendLine($"Make: {this.Make}");
+            sb.AppendLine($"Model: {this.Model}");
+            sb.AppendLine($"HorsePower: {this.HorsePower}");
+            sb.Append($"RegistrationNumber: {this.RegistrationNumber}");
+            
+            return sb.ToString();
+        }
+    }
+}
---
+++ DefiningClasses/SoftUniParking/Parking.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SoftUniParking
+{
+    public class Parking
+    {
+        private List<Car> cars;
+        private int count;
+        public int Count
+        {
+            get { return this.cars.Count; }
+            
+        }
+
+         
+        private int capacity;
+        public int Capacity
+        {
+            get { return capacity; }
+            set { capacity = value; }
+        }
+      
+        public Parking(int capacity)
+        {
+            this.cars = new List<Car>();
+            this.Capacity = capacity;
+        }
+
+        public string AddCar(Car car)
+        {
+
+            if (this.cars.Find(c => c.RegistrationNumber == car.RegistrationNumber) != null)
+            {
+                return $"Car with that registration number, already exists!";
+            }
+            else
+            {
+                if (this.cars.Count >= this.Capacity)
+                {
+                    return $"Parking is full!";
+                }
+                else
+                {
+                    this.cars.Add(car);
+                    return $"Successfully added new car { car.Make } { car.RegistrationNumber}";
+                }
+            }
+         
+        }
+
+        public string RemoveCar(string registrationNumber)
+        {
+            if (this.cars.Any(c => c.RegistrationNumber == registrationNumber))
+            {
+                Car currCar = this.cars.Find(c => c.RegistrationNumber == registrationNumber);
+                this.cars.Remove(currCar);
+                return $"Successfully removed {currCar.RegistrationNumber}";
+            }
+            else
+            {
+                return $"Car with that registration number, doesn't exist!";
+            }
+        }
+
+        public Car GetCar(string registrationNumber)
+        {
+            return this.cars.Find(c => c.RegistrationNumber == registrationNumber);
+        }
+
+        public void RemoveSetOfRegistrationNumber(List<string> registrationNumbers)
+        {
+            foreach (string regNum in registrationNumbers)
+            {
+                while (this.cars.Any(c => c.RegistrationNumber == regNum))
+                {
+                    Car currCar = this.cars.Find(c => c.RegistrationNumber == regNum);
+                    this.cars.Remove(currCar);
+                }
+            }
+        }
+    }
+}
