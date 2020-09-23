0d991b0fe27bb8a4aa67310224cf62fec3122388
Aleksandra Toncheva <aleksandra_toncheva@yahoo.com>
2020-09-23 17:54:56 +0300

    Add files to HackerRank.sln

---
+++ HackerRank/IcaCreamParlor/Program.cs
@@ -0,0 +1,65 @@
+﻿using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Solution
+{
+
+    // Complete the icecreamParlor function below.
+    static int[] icecreamParlor(int m, int[] arr)
+    {
+        var result = new int[2];
+        var prices = new Dictionary<int, int>();
+        for (var i = 1; i <= arr.Length; i++)
+        {
+            var diff = m - arr[i - 1];
+            if (prices.ContainsKey(diff))
+            {
+                result[0] = prices[diff];
+                result[1] = i;
+                
+                break;
+            }
+            else
+            {
+                prices.Add(arr[i - 1], i);
+            }
+        }
+        Array.Sort(result);
+        return result;
+       
+    }
+
+    static void Main(string[] args)
+    {
+        //TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+        int t = Convert.ToInt32(Console.ReadLine());
+
+        for (int tItr = 0; tItr < t; tItr++)
+        {
+            int m = Convert.ToInt32(Console.ReadLine());
+
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            int[] arr = Array.ConvertAll(Console.ReadLine().Split(' '), arrTemp => Convert.ToInt32(arrTemp))
+            ;
+            int[] result = icecreamParlor(m, arr);
+
+            Console.WriteLine(string.Join(" ", result));
+        }
+
+        //textWriter.Flush();
+        //textWriter.Close();
+    }
+}
---
+++ HackerRank/VowelSubstring/Program.cs
@@ -0,0 +1,189 @@
+﻿//using System.CodeDom.Compiler;
+//using System.Collections.Generic;
+//using System.Collections;
+//using System.ComponentModel;
+//using System.Diagnostics.CodeAnalysis;
+//using System.Globalization;
+//using System.IO;
+//using System.Linq;
+//using System.Reflection;
+//using System.Runtime.Serialization;
+//using System.Text.RegularExpressions;
+//using System.Text;
+//using System;
+
+
+
+//class Result
+//{
+
+//    /*
+//     * Complete the 'findSubstring' function below.
+//     *
+//     * The function is expected to return a STRING.
+//     * The function accepts following parameters:
+//     *  1. STRING s
+//     *  2. INTEGER k
+//     */
+
+//    public static string findSubstring(string s, int k)
+//    {
+//        string result = string.Empty;
+//        int maxCount = 0;
+
+
+//        int i = 0;
+//        if (!s.Contains('a') && !s.Contains('o') && !s.Contains('e') && !s.Contains('i') && !s.Contains('u'))
+//        {
+//            return "Not found!";
+//        }
+//        else
+//        {
+//            while (i + k <= s.Length)
+//            {
+
+//                string curr = s.Substring(i, k);
+//                //List<char> arr = new List<char>();
+//                //arr = curr.ToArray().Where(c => c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u').ToList();
+//                int j = 0;
+//                int count = 0;
+//                while (j<curr.Length)
+//                {
+//                    char c = curr[j];
+//                    if(c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u')
+//                    {
+//                        count++;
+//                    }
+//                    if(count>maxCount)
+//                    {
+//                        maxCount = count;
+//                        result=curr;
+//                    }
+//                }
+
+//                i++;
+//            }
+//            return result;
+//        }
+
+
+//    }
+
+//}
+
+
+
+//class Solution
+//{
+//    public static void Main(string[] args)
+//    {
+//        //TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+//        string s = Console.ReadLine();
+
+//        int k = Convert.ToInt32(Console.ReadLine().Trim());
+
+//        string result = Result.findSubstring(s, k);
+
+//        Console.WriteLine(result);
+
+//        //textWriter.Flush();
+//        //textWriter.Close();
+//    }
+//}
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+
+
+class Result
+{
+
+    /*
+     * Complete the 'findSubstring' function below.
+     *
+     * The function is expected to return a STRING.
+     * The function accepts following parameters:
+     *  1. STRING s
+     *  2. INTEGER k
+     */
+
+    public static string findSubstring(string s, int k)
+    {
+        string result = string.Empty;
+        int maxCount = 0;
+
+
+        int i = 0;
+        if (!s.Contains('a') && !s.Contains('o') && !s.Contains('e') && !s.Contains('i') && !s.Contains('u'))
+        {
+            return "Not found!";
+        }
+        else
+        {
+            while (i + k <= s.Length)
+            {
+
+                string curr = s.Substring(i, k);
+                //List<char> arr = new List<char>();
+                //arr = curr.ToArray().Where(c => c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u').ToList();
+                int j = 0;
+                int count = 0;
+                while (j < curr.Length)
+                {
+                    char c = curr[j];
+                    if (c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u')
+                    {
+                        count++;
+                    }
+
+                    j++;
+                }
+                if (count > maxCount)
+                {
+                    maxCount = count;
+                    result = curr;
+                }
+
+                i++;
+            }
+            return result;
+        }
+
+
+
+    }
+}
+
+
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+        TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+        string s = Console.ReadLine();
+
+        int k = Convert.ToInt32(Console.ReadLine().Trim());
+
+        string result = Result.findSubstring(s, k);
+
+        textWriter.WriteLine(result);
+
+        textWriter.Flush();
+        textWriter.Close();
+    }
+}
+
