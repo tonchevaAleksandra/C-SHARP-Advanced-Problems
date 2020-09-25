85a00e639de01f3d44664ae2a9157d931bfdf2a5
Aleksandra Toncheva <aleksandra_toncheva@yahoo.com>
2020-09-26 00:00:54 +0300

    Add files to Dictionary

---
+++ SetsAndDictionariesAdvanced/Shop/Program.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Text;
+
+namespace Sets
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            int count= 1000000;
+            int containsCount = 100000;
+            List<string> list = new List<string>(count);
+            HashSet<string> hash = new HashSet<string>(count);
+            StringHashSet ourHash = new StringHashSet(count);
+
+            for (int i = 0; i < count; i++)
+            {
+                list.Add(i.ToString());
+                hash.Add(i.ToString());
+                ourHash.Add(i.ToString());
+            }
+            Console.WriteLine("Start");
+
+            var timer = Stopwatch.StartNew();
+            for (int i = 0; i < containsCount; i++)
+            {
+                bool exist = list.Contains(i.ToString());
+            }
+            timer.Stop();
+            Console.WriteLine("Timer: List");
+            Console.WriteLine(timer.ElapsedMilliseconds);
+            timer.Reset();
+            timer.Start();
+            
+            for (int i = 0; i < containsCount; i++)
+            {
+                bool exist = hash.Contains(i.ToString());
+            }
+            timer.Stop();
+            Console.WriteLine("Timer HashSet:");
+            Console.WriteLine(timer.ElapsedMilliseconds);
+            timer.Reset();
+            timer.Start();
+           
+            for (int i = 0; i < containsCount; i++)
+            {
+                bool exist = ourHash.Contains(i.ToString());
+            }
+            timer.Stop();
+            Console.WriteLine("Timer - our implementation of HashSet:");
+            Console.WriteLine(timer.ElapsedMilliseconds);
+            timer.Reset();
+            timer.Start();
+            //            StringHashSet set = new StringHashSet(8);
+
+            //            set.Add("hey");
+            //            set.Add("6");
+            //            set.Add("7");
+            //            set.Add("fsd");
+            //            set.Add("oshte");
+            //            set.Add("g");
+            //            set.Add("6");
+            //            set.Add("33");
+            //            set.Add("opa");
+
+
+            //            Console.WriteLine(set.Contains("33"));
+            //            Console.WriteLine(set.Contains("opa"));
+            //            Console.WriteLine(set.Contains("ff"));
+            //            Console.WriteLine
+            //(set.Contains("heyy"));
+
+
+        }
+
+        static string GenerateString(int length)
+        {
+            StringBuilder sb = new StringBuilder();
+            Random rand = new Random();
+            for (int i = 0; i < length; i++)
+            {
+                sb.Append(rand.Next(0, 9));
+            }
+
+            return sb.ToString();
+        }
+    }
+}
---
+++ SetsAndDictionariesAdvanced/Shop/StringHashSet.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Sets
+{
+    class SetElement
+    {
+        public List<string> Keys  { get; set; }
+
+        public override int GetHashCode()
+        {
+            return base.GetHashCode();  
+        }
+
+    }
+    public class StringHashSet
+    {
+        private SetElement[] array;
+        
+
+        public StringHashSet()
+        {
+
+        }
+        public StringHashSet(int capacity=8)
+        {
+            array = new SetElement[capacity];
+        }
+
+        public void Add(string key)
+        {
+            int index = HashFunction(key);
+            if (array[index] != null)
+            {
+                array[index].Keys.Add(key);
+
+            }
+            else
+            {
+                array[index] = new SetElement() { Keys = new List<string>() { key } };
+            }
+           
+        }
+
+        private int HashFunction(string key)
+        {
+            UInt64 hashedValue = 3074457345618258791ul;
+            for (int i = 0; i < key.Length; i++)
+            {
+                hashedValue +=key[i];
+
+                hashedValue *= 3074457345618258799ul;
+            }
+            UInt64 count = (UInt64)array.Length;
+            return (int) (hashedValue % count);
+
+                /*key[0] << 5 | key[key.Length - 1] >> 3 % array.Length;*/
+            //return Math.Abs(key.GetHashCode() % array.Length);
+            //int asciiResult = key[0] << 5 | key[key.Length - 1] >> 4;
+
+            //int asciiResult = 0;
+            //for (int i = 0; i < key.Length; i++)
+            //{
+            //    asciiResult += key[i];
+            //}
+            return asciiResult % key.Length;
+        }
+
+        public bool Contains(string key)
+        {
+            int index = HashFunction(key);
+            if (array[index]!= null)
+            {
+                for (int i = 0; i < array[index].Keys.Count; i++)
+                {
+                    if(array[index].Keys[i]==key)
+                    {
+                        return true;
+                    }
+                }
+            }
+              
+            return false;
+        }
+    }
+}
