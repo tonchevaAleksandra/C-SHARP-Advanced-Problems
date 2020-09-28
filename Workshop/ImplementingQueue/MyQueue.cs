668349efc99879672ce72f3fbaf80a419b173f41
Aleksandra Toncheva <aleksandra_toncheva@yahoo.com>
2020-09-28 20:20:59 +0300

    Add files to Workshop

---
+++ Workshop/ImplementingQueue/MyQueue.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ImplementingQueue
+{
+    public class MyQueue<T>:IEnumerable<T>
+    {
+        private int capacity;
+        private T[] data;
+
+        public MyQueue()
+           : this(4)
+        {
+
+        }
+
+        public MyQueue(int capacity)
+        {
+            this.capacity = capacity;
+            this.data = new T[capacity];
+        }
+
+       
+
+        public int Count{ get; private set; }
+        public MyQueue(T[] collection):
+            this(collection.Length)
+        {
+
+            foreach (var item in collection)
+            {
+                this.Enqueue(item);
+            }
+        }
+        public void Enqueue(T element)
+        {
+            if (this.Count == this.data.Length)
+            {
+                this.Resize();
+            }
+            this.data[this.Count] = element;
+
+            this.Count++;
+        }
+
+        public T Dequeue()
+        {
+            this.ValidateIfQueueIsEmpty();
+            var currElement = this.data[0];
+            this.Count--;
+            return currElement;
+        }
+
+        public T Peek()
+        {
+            this.ValidateIfQueueIsEmpty();
+
+            return this.data[0];
+
+        }
+        public void Clear()
+        {
+            this.Count = 0;
+            this.data = new T[capacity];
+        }
+
+        public void ValidateIfQueueIsEmpty()
+        {
+            if(this.Count==0)
+            {
+                throw new InvalidOperationException("The queue is empty. You can not make this operation on an empty queue!");
+            }
+        }
+
+        public void Resize()
+        {
+            var newCapacity = this.data.Length * 2;
+            var newData = new T[newCapacity];
+
+            for (int i = 0; i < this.data.Length; i++)
+            {
+                newData[i] = this.data[i];
+            }
+
+            this.data = newData;
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        => this.data
+            .Take(this.Count) 
+            .ToList()
+            .GetEnumerator();
+
+        IEnumerator IEnumerable.GetEnumerator()
+       => this.GetEnumerator();
+
+
+        public override string ToString()
+        {
+            return this.data.ToString(); 
+        }
+    }
+}
---
+++ Workshop/ImplementingQueue/StartUp.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+using System.Collections.Generic;
+
+namespace ImplementingQueue
+{
+   public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            var queue = new MyQueue<int>();
+            for (int i = 0; i < 6; i++)
+            {
+                queue.Enqueue(i+1);
+            }
+
+            string[] collection = new string[10];
+            for (int i = 0; i < 10; i++)
+            {
+                collection[i] = $"some {i}";
+            }
+
+            var firstQueue = new MyQueue<string>(collection);
+            Console.WriteLine(firstQueue.Peek());
+            //firstQueue.Foreach(Console.WriteLine);
+        
+        }
+    }
+}
