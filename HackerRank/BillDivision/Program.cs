23fc4009ceca749b7fad5a833617a3b38661e9b9
Aleksandra Toncheva <aleksandra_toncheva@yahoo.com>
2020-10-04 19:33:55 +0300

    Add files to Hackerrank

---
+++ HackerRank/BillDivision/Program.cs
@@ -0,0 +1,49 @@
+﻿using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Solution
+{
+
+    // Complete the bonAppetit function below.
+    static void bonAppetit(List<int> bill, int k, int b)
+    {
+         bill.RemoveAt(k);
+        var ordersAnna = bill.Sum();
+        int diff = Math.Abs(ordersAnna / 2 - b);
+        if (diff==0)
+        {
+            Console.WriteLine("Bon Appetit");
+        }
+        else
+        {
+            Console.WriteLine(diff);
+        }
+
+    }
+
+    static void Main(string[] args)
+    {
+        string[] nk = Console.ReadLine().TrimEnd().Split(' ');
+
+        int n = Convert.ToInt32(nk[0]);
+
+        int k = Convert.ToInt32(nk[1]);
+
+        List<int> bill = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(billTemp => Convert.ToInt32(billTemp)).ToList();
+
+        int b = Convert.ToInt32(Console.ReadLine().Trim());
+
+        bonAppetit(bill, k, b);
+    }
+}
