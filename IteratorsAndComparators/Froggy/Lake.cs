3e160bd722dca6d7b08f9dee8adb1e71278ff4f6
Aleksandra Toncheva <aleksandra_toncheva@yahoo.com>
2020-10-07 14:23:32 +0300

    Add files to IteratorsAndComparators

---
+++ IteratorsAndComparators/Froggy/Lake.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Froggy
+{
+    public class Lake : IEnumerable<int>
+    {
+        private readonly int[] stones;
+
+        public Lake(params int[] stones)
+        {
+            this.stones = stones;
+        }
+
+        public IEnumerator<int> GetEnumerator()
+        {
+            for (int i = 0; i < stones.Length; i+=2)
+            {
+                yield return this.stones[i];
+            }
+            for (int i = stones.Length-1 ; i >= 0 ; i--)
+            {
+                if(i%2!=0)
+                {
+                    yield return this.stones[i];
+                }
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return this.GetEnumerator();
+        }
+    }
+}
---
+++ IteratorsAndComparators/Froggy/StartUp.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Linq;
+
+namespace Froggy
+{
+   public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            int[] stones = Console.ReadLine()
+            .Split(", ", StringSplitOptions.RemoveEmptyEntries)
+            .Select(int.Parse)
+            .ToArray();
+            Lake lake = new Lake(stones);
+            //Lake lake = new Lake(new int[] { 1, 2, 3, 4, 5 });// this would work just with constructor with params
+            Console.WriteLine(string.Join(", ",lake));
+        }
+    }
+}
