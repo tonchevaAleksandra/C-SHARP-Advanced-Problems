6e8e07b129516a5a225f23c20e5dea40b936b92b
Aleksandra Toncheva <aleksandra_toncheva@yahoo.com>
2020-10-07 18:04:35 +0300

    Add file to BasicAlgorithms

--- BasicAlgorithms/RecursiveFactorial/Program.cs
+++ BasicAlgorithms/RecursiveFactorial/Program.cs
@@ -6,7 +6,18 @@ namespace RecursiveFactorial
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int n = int.Parse(Console.ReadLine());
+            Console.WriteLine(Factorial(n));
+        }
+
+        static int Factorial(int n)
+        {
+            if (n == 0)
+            {
+                return 1;
+            }
+
+            return n * Factorial(n - 1);
         }
     }
 }
