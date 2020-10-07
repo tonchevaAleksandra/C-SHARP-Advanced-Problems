3cb8b9bafc56f5a3a9b26ed3531a3020945aa88c
Aleksandra Toncheva <aleksandra_toncheva@yahoo.com>
2020-10-07 17:49:35 +0300

    Add files to BasicAlgorithms

---
+++ BasicAlgorithms/RecursiveArraySum/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Linq;
+
+namespace RecursiveArraySum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int[] nums = Console.ReadLine()
+            .Split()
+            .Select(int.Parse)
+            .ToArray();
+            Console.WriteLine(Sum(nums, 0));
+        }
+
+        static int Sum(int[] nums, int index)
+        {
+            if (index == nums.Length)
+            {
+                return 0;
+            }
+            int result = Sum(nums, index + 1);
+            return nums[index] + result;
+        }
+    }
+}
