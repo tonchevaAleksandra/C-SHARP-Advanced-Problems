f766468259c576bc22c232af6f0973f2e7cafa16
Aleksandra Toncheva <aleksandra_toncheva@yahoo.com>
2020-10-09 01:29:23 +0300

    Add files to HackerRank

---
+++ HackerRank/MarcAndToys/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Linq;
+
+namespace MarcAndToys
+{
+    class Program
+    {
+        static int maximumToys(int[] prices, int k)
+        {
+            prices = prices.OrderBy(x => x).ToArray();
+            var sum = 0;
+            int result = 0;
+            for (int i = 0; i < prices.Length; i++)
+            {
+                sum += prices[i];
+                if(sum>k)
+                {
+             
+                    result = i;
+                    break;
+                }
+            }
+            return result;
+        }
+        static void Main(string[] args)
+        {
+            string[] nk = Console.ReadLine().Split(' ');
+
+            int n = Convert.ToInt32(nk[0]);
+
+            int k = Convert.ToInt32(nk[1]);
+
+            int[] prices = Array.ConvertAll(Console.ReadLine().Split(' '), pricesTemp => Convert.ToInt32(pricesTemp))
+            ;
+            int result = maximumToys(prices, k);
+
+            Console.WriteLine(result);
+        }
+    }
+}
