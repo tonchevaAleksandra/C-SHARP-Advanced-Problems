24f3505cabf9550843252c1dd8bb8afacc2d7c9d
Aleksandra Toncheva <aleksandra_toncheva@yahoo.com>
2020-10-08 16:15:53 +0300

    Add files to HackerRank - Greedy Algorithm

---
+++ HackerRank/MinimumAbsoluteDifferenceInAnArray/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Linq;
+
+namespace MinimumAbsoluteDifferenceInAnArray
+{
+    class Program
+    {
+        static int minimumAbsoluteDifference(int[] arr)
+        {
+
+            int minDiff = int.MaxValue;
+
+            arr = arr.OrderByDescending(x => x).ToArray();
+            int j = 1;
+            for (int i = 0; i < arr.Length - 1; i++)
+            {
+                if (Math.Abs(arr[i] - arr[j]) < minDiff)
+                {
+                    minDiff = Math.Abs(arr[i] - arr[j]);
+                }
+                j++;
+            }
+            return minDiff;
+        }
+
+        static void Main(string[] args)
+        {
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            int[] arr = Array.ConvertAll(Console.ReadLine().Split(' '), arrTemp => Convert.ToInt32(arrTemp))
+            ;
+            int result = minimumAbsoluteDifference(arr);
+
+            Console.WriteLine(result);
+
+        }
+
+    }
+}
