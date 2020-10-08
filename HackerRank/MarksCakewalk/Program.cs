533e306292af727522ab6013d915c38248cdaa50
Aleksandra Toncheva <aleksandra_toncheva@yahoo.com>
2020-10-08 17:28:19 +0300

    Add files to HackerRank - Greedy Algorithm

---
+++ HackerRank/MarksCakewalk/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Linq;
+
+namespace MarksCakewalk
+{
+    class Program
+    {
+        static long marcsCakewalk(int[] calorie)
+        {
+           calorie = calorie.OrderByDescending(x => x).ToArray();
+           long result = 0;
+            for (int i = 0; i < calorie.Length; i++)
+            {
+                var power = Math.Pow(2, i);
+                result += (long)((power) * calorie[i]);
+            }
+            return result;
+        }
+        static void Main(string[] args)
+        {
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            int[] calorie = Array.ConvertAll(Console.ReadLine().Split(' '), calorieTemp => Convert.ToInt32(calorieTemp))
+            ;
+            long result = marcsCakewalk(calorie);
+
+            Console.WriteLine(result);
+        }
+    }
+}
