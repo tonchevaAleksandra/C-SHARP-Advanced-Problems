32d43150f7188e414948a018382d3bda67700b86
Aleksandra Toncheva <aleksandra_toncheva@yahoo.com>
2020-10-09 00:23:27 +0300

    Add files to HackerRank-Greedy

---
+++ HackerRank/AngryProfessor/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Linq;
+
+namespace AngryProfessor
+{
+    class Program
+    {
+        static string angryProfessor(int k, int[] a)
+        {
+           
+            return k <= a.Where(x => x <= 0).Count() ? "NO" : "YES";
+
+        }
+        static void Main(string[] args)
+        {
+            int t = Convert.ToInt32(Console.ReadLine());
+
+            for (int tItr = 0; tItr < t; tItr++)
+            {
+                string[] nk = Console.ReadLine().Split(' ');
+
+                int n = Convert.ToInt32(nk[0]);
+
+                int k = Convert.ToInt32(nk[1]);
+
+                int[] a = Array.ConvertAll(Console.ReadLine().Split(' '), aTemp => Convert.ToInt32(aTemp))
+                ;
+                string result = angryProfessor(k, a);
+
+                Console.WriteLine(result);
+            }
+        }
+    }
+}
