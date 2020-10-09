05522447c51a67da2d98ca25a2ef36fb4b957b35
Aleksandra Toncheva <aleksandra_toncheva@yahoo.com>
2020-10-09 20:57:33 +0300

    Add files to HackerRank

---
+++ HackerRank/JimAndTheOrders/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace JimAndTheOrders
+{
+    class Program
+    {
+        static int[] jimOrders(int[][] orders)
+        {
+            var dict = new Dictionary<int, int>();
+            for (int i = 0; i < orders.GetLength(0); i++)
+            {
+                var currTime = orders[i][0] + orders[i][1];
+                dict.Add(i + 1, currTime);
+            }
+            return dict.OrderBy(x => x.Value).ThenBy(x => x.Key).Select(x => x.Key).ToArray();
+        }
+        static void Main(string[] args)
+        {
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            int[][] orders = new int[n][];
+
+            for (int i = 0; i < n; i++)
+            {
+                orders[i] = Array.ConvertAll(Console.ReadLine().Split(' '), ordersTemp => Convert.ToInt32(ordersTemp));
+            }
+
+            int[] result = jimOrders(orders);
+
+            Console.WriteLine(string.Join(" ", result));
+        }
+    }
+}
---
+++ HackerRank/PermuttingTwoArrays/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Linq;
+
+namespace PermutingTwoArrays
+{
+    class Program
+    {
+        static string twoArrays(int k, int[] A, int[] B)
+        {
+            A = A.OrderBy(x => x).ToArray();
+            B = B.OrderByDescending(x => x).ToArray();
+            for (int i = 0; i < A.Length; i++)
+            {
+                if(A[i]+B[i]<k)
+                {
+                    return "NO";
+                }
+            }
+            return "YES";
+        }
+        static void Main(string[] args)
+        {
+            int q = Convert.ToInt32(Console.ReadLine());
+
+            for (int qItr = 0; qItr < q; qItr++)
+            {
+                string[] nk = Console.ReadLine().Split(' ');
+
+                int n = Convert.ToInt32(nk[0]);
+
+                int k = Convert.ToInt32(nk[1]);
+
+                int[] A = Array.ConvertAll(Console.ReadLine().Split(' '), ATemp => Convert.ToInt32(ATemp))
+                ;
+
+                int[] B = Array.ConvertAll(Console.ReadLine().Split(' '), BTemp => Convert.ToInt32(BTemp))
+                ;
+                string result = twoArrays(k, A, B);
+
+                Console.WriteLine(result);
+            }
+        }
+    }
+}
