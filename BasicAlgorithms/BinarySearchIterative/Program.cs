0e8e7c9b2e47cbbaa2a50b166004f83bd846c023
Aleksandra Toncheva <aleksandra_toncheva@yahoo.com>
2020-10-11 17:26:50 +0300

    Add files to Algorithms

---
+++ BasicAlgorithms/BinarySearchIterative/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace BinarySearchIterative
+{
+    class Program
+    {
+       static int BinarySearch(int[] arr, int key, int start, int end)
+        {
+            while (end>=start)
+            {
+                int mid = (start + end) / 2;
+                if(arr[mid]<key)
+                {
+                    start = mid + 1;
+                }
+                else if(arr[mid]>key)
+                {
+                    end = mid - 1;
+                }
+                else
+                {
+                    return mid;
+                }
+            }
+
+            return -1;
+        }
+        static void Main(string[] args)
+        {
+            var collection = new int[] { 15, 7, 2, 4, 18, 1, 53, 35, 5, 4, 2, 87, 14, 13, 16, 81 };
+            Array.Sort(collection);
+            int start = 0;
+            int end = collection.Length-1;
+            int key = int.Parse(Console.ReadLine());
+            Console.WriteLine($"Index of searched element if {BinarySearch(collection,key, start, end)}");
+
+        }
+    }
+}
---
+++ BasicAlgorithms/SelectionSort/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SelectionSort
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var collection = new int[] { 15, 7, 2, 4, 18, 1, 53, 35, 5, 4, 2, 87, 14, 13, 16, 81 };
+            Print(collection);
+            for (int i = 0; i < collection.Length; i++)
+            {
+                int min = i;
+                
+                for (int j = i+1; j < collection.Length; j++)
+                {
+                    if (Less(collection[j],collection[min]))
+                    {
+                        min = j;
+                    }
+                }
+                Swap(collection, i, min);
+                Print(collection);
+            }
+           
+        }
+
+        private static void Print(int[] collection)
+        {
+            Console.WriteLine(string.Join(" ", collection));
+        }
+
+        private static int[] Swap(int[] collection, int i, int min)
+        {
+            var temp = collection[i] ;
+            collection[i] = collection[min];
+            collection[min] = temp;
+            return collection;
+        }
+
+        private static bool Less(int first, int second)
+        {
+            return first < second ? true : false;
+        }
+    }
+}
