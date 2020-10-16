a87dce08625eb4b134eb9815a7c935eb9c45d655
Aleksandra Toncheva <aleksandra_toncheva@yahoo.com>
2020-10-16 17:54:25 +0300

    Add files to Exams

---
+++ RetakeExam17December2019/Christmas/Bag.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Christmas
+{
+  public  class Bag
+    {
+        private List<Present> data;
+
+        public string Color { get; set; }
+        public int Capacity { get; set; }
+        public int Count 
+        {
+            get
+            {
+                return this.data.Count;
+            }
+        }
+        public Bag(string color, int capacity)
+        {
+            this.Color = color;
+            this.Capacity = capacity;
+            this.data = new List<Present>();
+        }
+
+        //•	Method Add(Present present) - adds an entity to the data if there is room for it
+        public void Add(Present present)
+        {
+            if(this.Count<this.Capacity)
+            {
+                this.data.Add(present);
+            }
+        }
+        //•	Method Remove(string name) - removes a present by given name, if such exists, and returns bool
+        public bool Remove(string name)
+        {
+            if (this.data.Any(x => x.Name == name))
+            {
+                Present currentPresent = this.data.FirstOrDefault(x => x.Name == name);
+                this.data.Remove(currentPresent);
+                return true;
+            }
+            return false;
+        }
+
+        //•	Method GetHeaviestPresent() - returns the heaviest present
+        public Present GetHeaviestPresent()
+        {
+            Present heaviest = this.data.OrderByDescending(p => p.Weight).FirstOrDefault();
+            return heaviest;
+        }
+        public Present GetPresent(string name)
+        {
+            Present present = this.data.FirstOrDefault(p => p.Name == name);
+            return present;
+        }
+        public string Report()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine($"{this.Color} bag contains:");
+            foreach (var item in this.data)
+            {
+                sb.AppendLine(item.ToString());
+            }
+
+            return sb.ToString().Trim();
+        }
+    }
+}
---
+++ RetakeExam17December2019/Christmas/Present.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Christmas
+{
+   public class Present
+    {
+        public string Name { get; set; }
+        public double Weight { get; set; }
+        public string Gender { get; set; }
+
+        public Present(string name, double weight, string gender)
+        {
+            this.Name = name;
+            this.Weight = weight;
+            this.Gender = gender;
+        }
+        public override string ToString()
+        {
+            return $"Present {this.Name} ({this.Weight}) for a {this.Gender}".ToString();
+        }
+    }
+}
