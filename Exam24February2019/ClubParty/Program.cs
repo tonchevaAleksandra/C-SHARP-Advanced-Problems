0cbed749335ea05bcead9bf51d23abd9c01bed92
Aleksandra Toncheva <aleksandra_toncheva@yahoo.com>
2020-10-18 15:15:49 +0300

    Add files to Exams

---
+++ Exam24February2019/ClubParty/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ClubParty
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int hallsMaxCapacity = int.Parse(Console.ReadLine());
+            var input = Console.ReadLine().Split().ToArray();
+            Stack<string> stack = new Stack<string>(input);
+            List<int> people = new List<int>();
+            Queue<char> halls = new Queue<char>();
+
+            int sum = 0;
+            while (stack.Any())
+            {
+                var currElement = stack.Pop();
+                if (char.IsLetter(currElement[0]))
+                {
+                    halls.Enqueue(char.Parse(currElement));
+
+                }
+                else
+                {
+                    if(!halls.Any())
+                    {
+                        continue;
+                    }
+                    if (sum+ int.Parse(currElement) > hallsMaxCapacity)
+                    {
+                        Console.WriteLine($"{halls.Dequeue()} -> {string.Join(", ", people)}");
+
+                        people.Clear();
+                        sum = 0;
+                    }
+                    if (halls.Any())
+                    {
+                        people.Add(int.Parse(currElement));
+                        sum += (int.Parse(currElement));
+                    }
+
+                }
+
+            }
+
+        }
+    }
+}
