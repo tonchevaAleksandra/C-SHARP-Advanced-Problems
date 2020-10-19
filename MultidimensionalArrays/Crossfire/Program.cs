187f19eed01b7fc5031a1e8e772a8c6e3d9ecde2
Aleksandra Toncheva <aleksandra_toncheva@yahoo.com>
2020-10-19 23:48:45 +0300

    Refactoring

---
+++ MultidimensionalArrays/Crossfire/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Linq;
+
+namespace Crossfire
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int[] targets = Console.ReadLine()
+             .Split()
+             .Select(int.Parse)
+             .ToArray();
+            int rows = targets[0];
+            int cols = targets[1];
+            int[,] matrix = new int[rows, cols];
+            FillTheMatrix(rows, cols, matrix);
+
+        }
+
+        private static void FillTheMatrix(int rows, int cols, int[,] matrix)
+        {
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < cols; j++)
+                {
+                    if (i == 0)
+                    {
+                        matrix[i, j] = j + 1;
+                    }
+                    else
+                    {
+                        matrix[i, j] = cols * i + j + 1;
+                    }
+
+                }
+            }
+        }
+    }
+}
