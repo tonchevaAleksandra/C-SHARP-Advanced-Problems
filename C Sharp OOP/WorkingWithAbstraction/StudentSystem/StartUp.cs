55b05d7634fed538148f6130c166fe85efbf8b45
Aleksandra Toncheva <aleksandra_toncheva@yahoo.com>
2020-10-20 17:57:49 +0300

    Add files to OOP

---
+++ C Sharp OOP/WorkingWithAbstraction/StudentSystem/StartUp.cs
@@ -0,0 +1,15 @@
+﻿namespace StudentSystem
+{
+   public class StartUp
+    {
+        static void Main()
+        {
+            var studentData = new StudentData();
+            var inputOutputProvider = new ConsoleInputOutputProvider();
+
+            var engine = new Engine(studentData, inputOutputProvider);
+
+            engine.Process();
+        }
+    }
+}
